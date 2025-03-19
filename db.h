017e6770ef13395dcac8093ec39fd6ffbdc08b28
Sam Miller <millersr25@vt.edu>
2025-03-18 23:01:32 -0400

    created files

---
+++ db.h
@@ -0,0 +1,95 @@
+#ifndef DB_H
+#define DB_H
+
+#include <string>
+
+class Database{
+    private: 
+        //member variable "db" is of string type
+        //ToDo
+        std::string db; 
+
+        //member variable "username" is of string type
+        //ToDo
+        std::string username; 
+
+        //member variable "password" is of string type
+        //ToDo
+        std::string password; 
+
+        //member variable "connected" is of boolean type
+        //The "connected" is set to false initially.
+        //ToDo
+        bool connected{false}; 
+
+        //member variable "instance" is static 
+        //"instance" (pointer to Database) is a staic variable that stores the instance of the database. Its value is set in the function "getInstance"
+        //ToDo
+        static Database* instance; 
+
+        //constructor that helps creating instance of db(e.g. sales.db) accepts name of the database, username, password. 
+        //ToDo
+        Database(const std::string& dname, const std::string& uname, const std::string& pword) 
+        : db{dname}, username{uname}, password{pword} {} 
+
+        //destructor that disconnects the connection if connected.
+        //ToDo
+        ~Database() {
+            if(connect) {
+                disconnect(); 
+            } 
+        } 
+
+    public: 
+        //"getInstance" that creates and returns the instance of the database (Database*). If called first time it sets the username and password. However, subsequent time, it matches the database name, username and password and returns the previous instance if matched else it throws std::runtime_error("invalid database name, username or password"). We are using Singleton Design Pattern that creates only one instance of the databse. The instance is still created by the constructor.
+        //ToDo
+        static Database& getInstance(const std::string& dname, const std::string& uname, const std::string& pword) {
+            if(nullptr == instance) {
+                instance = new Database(dname, uname, pword); 
+            }
+            
+            return *instance; 
+        }
+
+        //"connect" that sets "connected" to true (return void)
+        //ToDo
+        void connect() {
+            if(!connected) {
+                connected = true; 
+            }
+        }
+        
+        //"disconnect" that sets "connected" to false (return void)
+        //ToDo
+        void disconnect() {
+            if(connected) {
+                connected = false; 
+            }        
+        }
+
+        // retrun status of connected true/false (return bool)
+        //ToDo
+
+        //overload the new operator that allocates memory using malloc of given size and returns pointer of type void and prints " overloaded new " (cout is okay in this case). std::cout << "overloaded new ";
+        //If the memory allocation fails it should throw std::bad_alloc()
+        //ToDo
+        
+
+
+        //overload the delete operator that deallocates memory and prints "overloaded delete " (cout is okay in this). std::cout << "overloaded delete ";
+        //ToDo
+
+        //set_username and get_username for username
+        //ToDo
+        //ToDo
+
+        //set_password and get_password for password.
+        //ToDo
+        //ToDo
+
+        //The static "resetInstance" as defined below.
+        static void resetInstance();
+};
+
+
+#endif
